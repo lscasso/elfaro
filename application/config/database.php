2f962754622141ac57f1b86b68b51cbdeebfd028
lscasso <lscasso@gmail.com>
2014-04-26 06:41:52 -0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'le1982';
+$db['default']['password'] = 'password';
 $db['default']['database'] = 'elFaro';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
